076b66a4150503d4df659f51687f520746358408
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-12-30 00:17:48 +0000

    Added post list

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Post
+from posts.models import Post
+
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -8,17 +9,16 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
+        if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError(
-                'Image size larger than 2MB!'
+                'Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
             )
         if value.image.width > 4096:
             raise serializers.ValidationError(
-                'image width larger than 4096px'
-            )
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'image height is larger than 4096px'
+                'Image width larger than 4096px!'
             )
         return value
 
