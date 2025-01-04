fad260594293b98afa1adfa35c4cf1791db4ff56
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2025-01-04 01:45:21 +0000

    Set debug to true

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-taiwoogbonyomi-drfapi-8abylz6pu9u.ws.codeinstitute-ide.net', 'taiwo-drf-api.herokuapp.com']
 
