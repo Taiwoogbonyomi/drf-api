c7c191f76e310c69e1a937a73d043bb350e0befa
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2025-01-05 22:26:52 +0000

    Fix bug in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,9 +63,9 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = [('ALLOWED_HOST'),'8000-taiwoogbonyomi-drfapi-8abylz6pu9u.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = [ os.environ.get('ALLOWED_HOST'),'8000-taiwoogbonyomi-drfapi-8abylz6pu9u.ws.codeinstitute-ide.net']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-taiwoogbonyomi-drfapi-8abylz6pu9u.ws.codeinstitute-ide.net']
 
